19f148890b92bf85762f427883bdf6a4b19dc34a
djreyab <djreya.boyd@gmail.com>
2025-08-28 02:07:11 -0600

    removed testing code

--- day-17-start/quiz-game-start/main.py
+++ day-17-start/quiz-game-start/main.py
@@ -4,8 +4,5 @@ from data import question_data
 question_bank = []
 
 for item in question_data:
-    item
     entry = Question(item['text'],item['answer'])
     question_bank.append(entry)
-
-print(question_bank)
