bf41107eeecd05ce3f7378c7f6da7394b6477b1e
D'Jreya Boyd <37115281+DJreyaB@users.noreply.github.com>
2025-08-28 01:49:15 -0600

    updated text attribute

--- day-17-start/quiz-game-start/question_model.py
+++ day-17-start/quiz-game-start/question_model.py
@@ -1,5 +1,5 @@
 class Question:
 
-    def __init__(self, question, answer):
-        self.question = question
+    def __init__(self, text, answer):
+        self.text = text
         self.answer = answer
