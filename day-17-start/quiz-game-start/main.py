ab719b3b2d55a0462f0e78e7064da62778c0da5f
djreyab <djreya.boyd@gmail.com>
2025-08-30 01:42:48 -0600

    added next question and while loop but bug present, must fix

--- day-17-start/quiz-game-start/main.py
+++ day-17-start/quiz-game-start/main.py
@@ -1,8 +1,14 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for item in question_data:
-    entry = Question(item['text'],item['answer'])
+    question = item["text"]
+    response = item["answer"]
+    entry = Question(question,response)
     question_bank.append(entry)
+
+quiz = QuizBrain(question_bank)
+print(quiz.question_list)
--- day-17-start/quiz-game-start/quiz_brain.py
+++ day-17-start/quiz-game-start/quiz_brain.py
@@ -4,9 +4,18 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = quiz_list
 
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)    
+    
     def next_question(self):
-        cur_question = self.question_list[self.question_number]
-        self.question_list[cur_question["text"]]
-        user_guess = input(f"Q.1 {cur_question} (True/False)")
-        self.question_number +=1
-        return user_guess == cur_question["answer"]
+        while self.still_has_questions():
+            self.question_number
+            self.question_list
+            cur_question = self.question_list[self.question_number]
+            self.question_number +=1
+            cur_question.text
+            self.question_list[cur_question.text]
+            user_guess = input(f"Q.{self.question_number} {cur_question} (True/False)")
+            return user_guess == cur_question["answer"]
+    
